2521d626a6ee91668ed701973fe126108658404d
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-01-06 16:31:16 +0530

    feat: add api route to add a new menu item

--- backend/src/routes/menu.ts
+++ backend/src/routes/menu.ts
@@ -19,4 +19,48 @@ router.get("", async (req: Request, res: Response) => {
   }
 });
 
+router.post("", async (req: Request, res: Response) => {
+  try {
+    const { name, category, price, availability } = req.body;
+
+    if (!name) {
+      return res.status(400).json({
+        message: "Item name is required",
+      });
+    } else if (!category) {
+      return res.status(400).json({
+        message: "Item category is required",
+      });
+    } else if (!price) {
+      return res.status(400).json({
+        message: "Item price is required",
+      });
+    } else if (!availability) {
+      return res.status(400).json({
+        message: "Item availability is required",
+      });
+    }
+
+    const newItem = new Menu({
+      name,
+      category,
+      price,
+      availability,
+    });
+
+    const savedItem = await newItem.save();
+    console.log("added new menu item", savedItem);
+
+    res.status(201).json({
+      message: "new item addded",
+      savedItem,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "error adding new item",
+      error,
+    });
+  }
+});
+
 export default router;
