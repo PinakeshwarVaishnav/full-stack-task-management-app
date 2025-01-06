e2037059552641eab833166e169aa8c9476ba5df
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-01-06 16:51:52 +0530

    feat: add api route to update a menu item

--- backend/src/routes/menu.ts
+++ backend/src/routes/menu.ts
@@ -63,4 +63,54 @@ router.post("", async (req: Request, res: Response) => {
   }
 });
 
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const itemId = req.params.id;
+    const { name, category, price, availability } = req.body;
+
+    if (!name) {
+      return res.status(400).json({
+        message: "Item name is required",
+      });
+    } else if (!category) {
+      return res.status(400).json({
+        message: "Item category is required",
+      });
+    } else if (!price) {
+      return res.status(400).json({
+        message: "Item price is required",
+      });
+    } else if (!availability) {
+      return res.status(400).json({
+        message: "Item availability is required",
+      });
+    }
+
+    const updatedItem = await Menu.findByIdAndUpdate(
+      itemId,
+      {
+        name,
+        category,
+        price,
+        availability,
+      },
+      { new: true },
+    );
+
+    if (!updatedItem) {
+      return res.status(404).json({ error: "Item not found" });
+    }
+
+    res.status(200).json({
+      message: "updated menu item",
+      updatedItem,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "updating menu item failed",
+      error,
+    });
+  }
+});
+
 export default router;
